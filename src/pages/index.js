5e35d08b232b31d722fc49f134c5e73ff5635879
ruucm <ruucm.a@gmail.com>
2019-06-13 03:17:21 +0900

    add framer & styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,27 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Frame } from "framer"
+import styled from "styled-components"
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-columns: center;
+`
+
+const Column = styled.div`
+  background: green;
+  flex: 0 0 33.3%;
+  max-width: 33.3%;
+`
+
+const Content = styled.div`
+  background: pink;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
 
 const IndexPage = ({ data, ...props }) => {
   console.log("data", data)
@@ -11,12 +32,42 @@ const IndexPage = ({ data, ...props }) => {
   return (
     <Layout>
       <SEO title={pages[0].node.title} />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
+
+      <Row>
+        <Column>
+          <Content>
+            <h1>Hi people</h1>
+          </Content>
+        </Column>
+        <Column>
+          <Content>
+            <Frame
+              initial={{
+                top: -100,
+                opacity: 0,
+              }}
+              animate={{
+                top: 0,
+                opacity: 1,
+              }}
+            >
+              <p>Welcome to your new Gatsby site.</p>
+              <p>Now go build something great.</p>
+            </Frame>
+          </Content>
+        </Column>
+        <Column>
+          <Content>3</Content>
+        </Column>
+        <Column>
+          <Content>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+              <Image />
+            </div>
+          </Content>
+        </Column>
+      </Row>
+
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
