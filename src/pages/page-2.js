bae40866eff23d4ecf6eea0a48bdadc2787691a9
ruucm <ruucm.a@gmail.com>
2019-06-13 03:47:56 +0900

    add pin datas

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,40 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = ({ data, ...props }) => {
+  const pins = data.allWordpressWpPin.edges
+
+  console.log("pins", pins)
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+
+      {pins.map((pin, id) => {
+        return <p>{pin.node.title}</p>
+      })}
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
+
+export const pageQuery = graphql`
+  query {
+    allWordpressWpPin {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          date
+        }
+      }
+    }
+  }
+`
